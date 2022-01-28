d1e88498ee187c34b9c4c2a7855fb00e034b2f7b
0xreeko <enric_trillo@outlook.com>
2022-01-28 10:45:21 +0000

    changed bg colour to wolven grey ish🐺

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ function App() {
         <meta name="revisit-after" content="1 days" />
         <meta name="language" content="en" />
       </Helmet>
-      <div className="flex flex-col w-full min-h-screen ">
-        <div className="bg-gray-900 h-12  w-full flex items-center px-4 underline text-white">
+      <div className="flex flex-col w-full min-h-screen bg-[#231F20]">">
+        <div className="h-12  w-full flex items-center px-4 underline text-white">
           <span>Hello BoilerplateV2</span>
         </div>
-        <div className="bg-gray-900 flex p-4 flex-grow w-full text-white">
+        <div className="flex p-4 flex-grow w-full text-white">
           <span>Server> Hello, No0bDev3l0per#3031</span>
         </div>
-        <div className="bg-black flex items-center p-4 h-12 w-full text-white text-sm">
+        <div className="flex items-center p-4 h-12 w-full text-white text-sm">
           <span>©2022 Developed by Twitter@0xreeko | Curated by {"<anon>"}</span>
         </div>
       </div>
