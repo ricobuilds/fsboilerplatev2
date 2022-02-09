6499297f0435e77600775f6112df634c5d4190b5
0xreeko <enric_trillo@outlook.com>
2022-02-09 17:37:18 +0000

    implemented useToggle to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
-import {useState} from 'react';
+import { useState } from "react";
 import "./index.css";
+import { useToggleHook } from "./hooks/useToggleHook";
 
 function App() {
-  let name: string = 'No0bDev3l0per#3031';
+  const [isToggle, toggle] = useToggleHook(false);
+
+  let name: string = "No0bDev3l0per#3031";
   let age: number = 1;
 
-  const [country, setCountry] = useState <string | null> ('UK');
+  const [country, setCountry] = useState<string | null>("UK");
 
   return (
+    <div className={`${isToggle ? "dark" : ""}`}>
       <div className="flex flex-col justify-center items-center w-full h-screen bg-[#231F20]">
         <div className="content">
-          <p className='text-white'>{name} is {age} years old and lives in {country}</p>
-          <input type="text" className="p-1 rounded outline-none" placeholder='enter your country' onChange={(e) => setCountry(e.target.value)}/>
+          <p className="text-white">
+            {name} is {age} years old and lives in {country}
+          </p>
+          <input
+            type="text"
+            className="p-1 rounded outline-none"
+            placeholder="enter your country"
+            onChange={(e) => setCountry(e.target.value)}
+          />
+          <div className="text-white" onClick={toggle}>
+            click me
+          </div>
         </div>
       </div>
+    </div>
   );
 }
 
