8e80dca781910a49acf70b350a78304c1cec4254
0xreeko <enric_trillo@outlook.com>
2022-02-10 00:02:37 +0000

    added localStorage to useToggle hook

--- src/hooks/useToggleHook.ts
+++ src/hooks/useToggleHook.ts
@@ -2,7 +2,10 @@ import { useState } from "react";
 export const useToggleHook = (initialValue: boolean): [boolean, () => void] => {
   const [value, setValue] = useState(initialValue);
 
-  const toggle = () => setValue(!value);
+  const toggle = () => {
+    setValue(!value);
+    localStorage.setItem("isDarkMode", value.toString());
+  };
 
   return [value, toggle];
 };
